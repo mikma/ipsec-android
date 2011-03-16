98dee71fbd00f7f54dd25961957c4d5eb1a467e1
Mikael Magnusson <mikma@users.sourceforge.net>
2011-03-16 21:11:59 +0100

    Copy assets to app_bin directory

--- src/org/za/hem/ipsec_tools/IPsecToolsActivity.java
+++ src/org/za/hem/ipsec_tools/IPsecToolsActivity.java
@@ -1,9 +1,19 @@
 package org.za.hem.ipsec_tools;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
+import android.widget.TextView;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -24,7 +34,10 @@ import android.view.View.OnClickListener;
  */
 
 public class IPsecToolsActivity extends Activity {
-
+	private File filesDir;
+	private TextView outputView;
+	private Handler handler = new Handler();
+	
 	/*
 	public String getLocalIpAddress() {
 	    try {
@@ -47,6 +60,10 @@ public class IPsecToolsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.ipsec_tools_activity);
+            filesDir = getDir("bin", MODE_PRIVATE);
+            putBinary("setkey");
+            putBinary("setkey.sh");
+            outputView = (TextView)findViewById(R.id.output);
             Button prefBtn = (Button) findViewById(R.id.pref_button);
             if (prefBtn != null)
             {
@@ -58,5 +75,65 @@ public class IPsecToolsActivity extends Activity {
                     }
             });
             }
+    		output(execute("/system/bin/ls " + new File(filesDir, "lib").getAbsolutePath()));
+    }
+    
+    /*
+     */
+    private void putBinary(String fileName) {
+    	try {
+    		File file = new File(filesDir, fileName);
+    		InputStream input = getAssets().open(fileName);
+    		int read;
+    		byte[] buffer = new byte[4096];
+    		OutputStream output = new FileOutputStream(file);
+    		
+    		while ((read = input.read(buffer)) > 0) {
+    			output.write(buffer, 0, read);
+    		}
+    		input.close();
+    		output.close();
+    		execute("/system/bin/chmod 711 " + file.getAbsolutePath());
+    	} catch (IOException e) {
+    		throw new RuntimeException(e);
+    	}
+    }
+    
+    private String execute(String cmd) {
+    	try {
+    		// Executes the command.
+    		Process process = Runtime.getRuntime().exec(cmd);
+        
+    		// Reads stdout.
+    		// NOTE: You can write to stdin of the command using
+    		//       process.getOutputStream().
+    		BufferedReader reader = new BufferedReader(
+    				new InputStreamReader(process.getInputStream()));
+    		int read;
+    		char[] buffer = new char[4096];
+    		StringBuffer output = new StringBuffer();
+    		while ((read = reader.read(buffer)) > 0) {
+    			output.append(buffer, 0, read);
+    		}
+    		reader.close();
+        
+    		// Waits for the command to finish.
+    		process.waitFor();
+        
+    		return output.toString();
+    	} catch (IOException e) {
+    		throw new RuntimeException(e);
+    	} catch (InterruptedException e) {
+    		throw new RuntimeException(e);
+    	}
+    }
+    
+    private void output(final String str) {
+    	Runnable proc = new Runnable() {
+			public void run() {
+				outputView.setText(str);
+			}
+    	};
+    	handler.post(proc);
     }
 }
