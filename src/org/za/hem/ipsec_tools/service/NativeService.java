a369076ad40aa1b2f289b991ab44bed0f4306149
Mikael Magnusson <mikma@users.sourceforge.net>
2011-07-16 14:59:51 +0200

    Move NativeService to package org.za.hem.ipsec_tools.service

--- src/org/za/hem/ipsec_tools/service/NativeService.java
+++ src/org/za/hem/ipsec_tools/service/NativeService.java
@@ -1,4 +1,4 @@
-package org.za.hem.ipsec_tools;
+package org.za.hem.ipsec_tools.service;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,6 +8,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetAddress;
 
+import org.za.hem.ipsec_tools.IPsecToolsActivity;
+import org.za.hem.ipsec_tools.R;
+import org.za.hem.ipsec_tools.R.drawable;
+import org.za.hem.ipsec_tools.R.string;
 import org.za.hem.ipsec_tools.racoon.Admin;
 import org.za.hem.ipsec_tools.racoon.Command;
 import org.za.hem.ipsec_tools.racoon.Event;
